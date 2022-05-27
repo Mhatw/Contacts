0e62e59ae9afe047d3f7af7a4cab5c1aaa075c0d
Frank Cano <frank.cano@utec.edu.pe>
2022-05-27 11:58:53 -0500

    ADD: CRUD contacts services

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-import {  getContacts } from "./scripts/services/contacts-services.js";
-import { login, logout } from "./scripts/services/sessions-service.js";
-
-login({email: "fabio@mail.com", password:"123456"})
-
-console.log(getContacts())
+// import {  createContacts, getContacts, deleteContacts, editContacts, showContact } from "./scripts/services/contacts-services.js";
+// import { login, logout } from "./scripts/services/sessions-service.js";
