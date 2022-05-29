d7b592842a2071c16cde1ea2fe193e930d235c6e
Frank Cano <frank.cano@utec.edu.pe>
2022-05-28 23:01:27 -0500

    import files to edit

---
+++ scripts/renders/editContact.js
@@ -0,0 +1,7 @@
+import DOMHandler from "../dom-handler.js";
+import { createContacts, editContacts } from "../services/contacts-services.js";
+import { logout } from "../services/sessions-service.js";
+import STORE from "../store.js";
+import { HomePage } from "./home.js";
+import loadingPage from "./loading.js";
+import LoginPage from "./login.js";
